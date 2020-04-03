8a18449218ef74e55198609bdf66ac5c8a98b431
Vishal Menon <vishalmenon@gmail.com>
2020-04-03 22:38:40 +0530

    added debian_basics to initialize a debian system. added node.JS module.

---
+++ node.JS/install_nodejs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+if [ $# -lt 1 ]
+then
+ printf "\nWrong Usage: Not enough arguments.\nusage:\n./install_nodejs.sh version_number \nTry 13 as version_number as of Fri, 03 Apr 2020 22:25:26 GMT\n\n\n"
+ exit 1
+fi
+
+curl -sL https://deb.nodesource.com/setup_"$1".x | sudo -E bash -
+
+printf "\n!!!Requiring root permission to install node.js\n\n"
+sudo apt-get install -y nodejs
+
+exit 0
