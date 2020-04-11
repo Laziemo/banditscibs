7578ffbeba922b3de471759e30c739bab7c26a72
Vishal Menon <vishalmenon@gmail.com>
2020-04-11 17:24:56 +0530

    removed call to get_debian_basics.sh from node.JS script to decouple scripts from system env

--- node.JS/install_nodejs.sh
+++ node.JS/install_nodejs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-bash ../system/get_debian_basics.sh
-
 if [ $# -lt 1 ]
 then
  printf "\nWrong Usage: Not enough arguments.\nusage:\n./install_nodejs.sh version_number \nTry 13 as version_number as of Fri, 03 Apr 2020 22:25:26 GMT\n\n\n"
