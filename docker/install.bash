f34d2e0660e09561f5b9d5ee6b6893de902f7b5f
Vishal Menon <vishalmenon.92@gmail.com>
2020-05-02 07:59:20 +0530

    updated docker install

--- docker/install.bash
+++ docker/install.bash
@@ -11,41 +11,35 @@ fi
 
 USER='$2'
 OS='$1'
-sudo apt update
 
+apt update
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg2 \
-    software-properties-common
 curl -fsSL https://download.docker.com/linux/$OS/gpg | sudo apt-key add -
 
-sudo apt-key fingerprint 0EBFCD88
+apt-key fingerprint 0EBFCD88
 
-sudo add-apt-repository \
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/$OS \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get update
+apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt install docker-ce docker-ce-cli containerd.io
 
 #TEST
-sudo docker run hello-world
+docker run hello-world
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-sudo docker-compose --version
+docker-compose --version
 
 
 #Create docker group to avoid using sudo
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+groupadd docker
+usermod -aG docker $USER
 
 exit 0
