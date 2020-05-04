01cd16cadbeff06d32d2dcba2236c27f9d435fbd
Vishal Menon <vishalmenon.92@gmail.com>
2020-05-05 01:30:41 +0530

    removed TEST section from docker install to keep install specialized

--- docker/install.bash
+++ docker/install.bash
@@ -27,18 +27,6 @@ apt update
 
 apt install docker-ce docker-ce-cli containerd.io
 
-#TEST
-docker run hello-world
-
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-chmod +x /usr/local/bin/docker-compose
-
-docker-compose --version
-
-
-#Create docker group to avoid using sudo
-
 groupadd docker
 usermod -aG docker $USER
 
