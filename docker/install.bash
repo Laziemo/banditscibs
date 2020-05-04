c5dfbfd5edbd09b6583d9c0fc2d03404144f945e
Vishal Menon <vishalmenon.92@gmail.com>
2020-05-05 01:50:55 +0530

    removed user_grp_add from docker/install for further specialization

--- docker/install.bash
+++ docker/install.bash
@@ -2,14 +2,13 @@
 
 #INSTALL 
 
-if [ $# -lt 2 ]
+if [ $# -lt 1 ]
 then
- printf "\nWrong Usage: Not enough arguments.\nusage:\n./install.sh os user_for_grpadd \nos must be ubuntu or debian. check about other  os.\nuser_for_grpadd is the name of a user to add to the docker group to allow them to run docker.\n\n"
+ printf "\nWrong Usage: Not enough arguments.\nusage:\n./install.sh <os>  \nos must be ubuntu or debian. check about other  os.\n\n"
  exit 1
 fi
 
 
-USER=$2
 OS=$1
 
 apt update
@@ -27,7 +26,5 @@ apt update
 
 apt install docker-ce docker-ce-cli containerd.io
 
-groupadd docker
-usermod -aG docker $USER
 
 exit 0
