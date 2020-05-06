a78f3046d1cd6035f84d3de1dccd5f731881143a
Vishal Menon <vishalmenon.92@gmail.com>
2020-05-06 16:25:34 +0530

    FOR MORE FLEXIBLE SETTINGS using nano instead of feeding a pub_key or sshd configurations.

--- system/init/setup_sudo_and_ssh_users.bash
+++ system/init/setup_sudo_and_ssh_users.bash
@@ -3,27 +3,28 @@
 
 if [ $# -lt 1 ]
 then
- printf "\nWrong Usage: Not enough arguments.\nusage:\n./setup_sudoand_ssh_users sudo_user sudo_pass ssh_key_name\nFirst two arguments are username and password for sudo user\nLast param is the ssh-key name for which the .pub will be placed in  /home/entry/.ssh/authorized_keys\n"
+ printf "\nWrong Usage: Not enough arguments.\nusage:\n./setup_sudo_and_ssh_users <sudo_user_name> \nFirst argument is admin username \n\n"
  exit 1
 fi
 
 
 
 sudo_user=$1
-ssh_pub_key=$(cat $HOME/.ssh/$2.pub)
 
 adduser "$sudo_user"
 usermod -aG sudo "$sudo_user"
+
 printf "Setting up user entry for ssh"
 adduser entry
 mkdir /home/entry/.ssh
 touch /home/entry/authorized_keys
-cat $ssh_pub_key >> /home/entry/authorized_keys
+nano /home/entry/authorized_keys
 
-echo "Port 22909" >> /etc/ssh/sshd_config
-echo "" >> /etc/ssh/sshd_config
-echo "" >> /etc/ssh/sshd_config
-echo "" >> /etc/ssh/sshd_config
+nano /etc/ssh/sshd_config
+#echo "Port 22909" >> /etc/ssh/sshd_config
+#echo "PermitRootLogin false" >> /etc/ssh/sshd_config
+#echo "" >> /etc/ssh/sshd_config
+#echo "" >> /etc/ssh/sshd_config
 
 echo "Access control setup complete."
 
