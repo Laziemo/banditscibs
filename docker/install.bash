a712bf29ebdedb8e8ec14aaeeb909d15a87f6500
Vishal Menon <vishalmenon.92@gmail.com>
2020-06-08 08:31:05 +0530

    usr/bin docker-compose FIX

--- docker/install.bash
+++ docker/install.bash
@@ -29,7 +29,7 @@ apt-get install -y \
  docker-ce-cli \
  containerd.io
 
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 
 chmod +x /usr/bin/docker-compose
 
