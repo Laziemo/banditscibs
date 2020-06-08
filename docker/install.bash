578f2338cf3467b59f1ffb1272b12dd66981fadd
Vishal Menon <vishalmenon.92@gmail.com>
2020-06-08 07:56:22 +0530

    changed docker-compose location to /usr/bin

--- docker/install.bash
+++ docker/install.bash
@@ -31,6 +31,6 @@ apt-get install -y \
 
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/bin/docker-compose
 
 exit 0
