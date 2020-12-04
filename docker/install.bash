fb346d4706ab79c340ad9f73a7475a1d6bde8568
ishi <vishalmenon.92@gmail.com>
2020-12-04 23:32:33 +0530

    docker install update

--- docker/install.bash
+++ docker/install.bash
@@ -1,26 +1,14 @@
 #!/bin/bash -e
 
-#INSTALL 
+# Install docker on debian
 
-if [ $# -lt 1 ]
-then
- printf "\nWrong Usage: Not enough arguments.\nusage:\n./install.sh <os>  \nos must be ubuntu or debian. check about other  os.\n\n"
- exit 1
-fi
-
-
-OS=$1
 
 apt-get update
-
-curl -fsSL https://download.docker.com/linux/$OS/gpg | apt-key add -
-
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 apt-key fingerprint 0EBFCD88
 
-#add-apt-repository \
-#   "deb [arch=amd64] https://download.docker.com/linux/$OS \
-#   $(lsb_release -cs) \
-#   stable"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs)  stable"
 
 apt-get update
 
