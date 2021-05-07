ea4349f5593e16bfd7f195ac072b69ea1765897b
Vishal Menon <tech@stackmate.in>
2021-05-07 15:07:07 +0530

    Signing Commit

--- certbot/genererate_certs.sh
+++ certbot/genererate_certs.sh
@@ -4,6 +4,8 @@
 # Buy a domain name at namecheap.com
 # Secure your domain name with cloudflare.com
 
+# Testing signed commits
+
 if [ $# -lt 1 ]
 then
  printf "\nWrong Usage: Not enough arguments. Missing url.\nusage:\n./generate_ssl_certs.sh <url-eg: mon.sats.cc> \nRegister and host a domain name (with namecheap/cloudflare) to resolve to this server's IP: use system/network/get_ip.sh to get your IP address\n\n\n"
